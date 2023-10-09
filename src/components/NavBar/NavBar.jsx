38b9162e376a998e2012dfd03f9a77f2d6baaff7
Jimena Hernández <her21199@uvg.edu.gt>
2023-10-09 14:37:49 -0600

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
     <header className="absolute inset-x-0 top-0 z-50 bg-je-blue">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-0 text-white font-extrabold text-[40px] leading-[48px] " style={{ fontFamily: 'Space Grotesk' }}>
+          <a href="#" className="-m-1.5 p-0 text-white font-bold text-[40px] leading-[48px] " style={{ fontFamily: 'Space Grotesk' }}>
             JE
           </a>
           </div>
@@ -69,7 +69,7 @@ const NavBar = () => {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-0 text-je-black font-extrabold text-[40px] leading-[48px] " style={{ fontFamily: 'Space Grotesk' }}>
+              <a href="#" className="-m-1.5 p-0 text-je-black font-bold text-[40px] leading-[48px] " style={{ fontFamily: 'Space Grotesk' }}>
                 JE
               </a>
               <button
