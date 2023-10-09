245817c2077c795abd7b2b8e51b867a8acf35926
Jimena Hernández <her21199@uvg.edu.gt>
2023-10-09 14:56:02 -0600

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
     <header className="absolute inset-x-0 top-0 z-50 bg-je-blue">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav className="flex items-center justify-between p-4 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-0 text-white font-bold text-[40px] leading-[48px] " style={{ fontFamily: 'Space Grotesk' }}>
             JE
